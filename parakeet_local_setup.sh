c01862eba0d776ecc82ac45cdc52c9669ed7e586
Joel Greer <jg13994@my.bristol.ac.uk>
2022-01-24 11:27:46 +0000

    Scripts for local setup of parakeet

---
+++ parakeet_local_setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# If doing first time setup, create a workdir and install python and clone
+# sp-sim repo using the python_local_setup.sh script
+
+# example use:
+# parakeet_local_setup.sh myworkdir
+#  where myworkdir is the sp-sim directory which you pulled
+
+# check you are on the branch you want to be on, then:
+
+# env variables for compilation
+export CXX=$(which g++)
+export CUDACXX=$(which nvcc)
+export CMAKE_CUDA_ARCHITECTURES=52 # CUDA arch for k80s on SCARF
+export FFTW_ROOT="/usr/local/lib/"
+
+# install and test...
+git clone https://github.com/rosalindfranklininstitute/amplus-digital-twin.git
+pushd amplus-digital-twin
+git submodule update --init --recursive
+pip install -r requirements.txt
+pip install -e .
+pytest
+popd
---
+++ python_local_setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+# example use:
+# python_local_setup.sh myworkdir pyenvname
+# If you are already in a workdir you have created should just be able to use:
+# python_local_setup.sh . pyenvname
+
+workdir="$1"
+pyenv="$2"
+
+popd
+
+mkdir "$workdir"
+pushd "$workdir"
+
+# if you haven't got python 3.9, install python 3.9
+# Setup up a venv you can use to install requirements via pip
+python3.9 -m venv "$pyenv"
+source "$pyenv/bin/activate"
+# "deactivate" is used to deactivate your python environment 
+
+pip3 install pytest
+pip3 install pytest-cov
+
+# if you didn't get this script via cloning you git repo now grab the sp-sim fork
+# git clone git@github.com:JatGreer/spsim.git
+
+# End of script
+# Now you should have a python env setup and activated
+# as well as the fork of sp-sim pulled to your work directory
