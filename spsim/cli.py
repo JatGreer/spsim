479a5e62f6c5eeda593fe7d6369e1e8da237bf1e
Joel Greer <jg13994@my.bristol.ac.uk>
2022-03-23 22:34:42 +0000

    Fixed the click cmd line and tested locally

--- spsim/cli.py
+++ spsim/cli.py
@@ -9,54 +9,66 @@ from humanize import naturaldelta
 
 from simulation_functions import prepare_simulation
 from utils import zarr2mrcs, json2star
+from localtesting import spsim_local_call
 
-@click.command()
+"""
+Example use of spsim_local via cmd line:
+python cli.py   spsim_local --input_directory=/home/jg/parakeet/testing/ --output_basename=/home/jg/parakeet/output_testing/test --n_images=20 --image_sidelength=800 --min_defocus=1 --max_defocus=10 --random_seed=6784 --n_gpus=1
+"""
+
+
+@click.group('cli')
+@click.pass_context
+def cli(command):
+    pass
+
+@cli.command('spsim_scarf')
 @click.option(
-    '--input-directory',
+    '--input_directory',
     type=click.Path(exists=True),
     prompt=True,
     help='input directory containing structure files'
 )
 @click.option(
-    '--output-basename',
+    '--output_basename',
     type=str,
     prompt=True,
     help='basename for output files from simulation'
 )
 @click.option(
-    '--n-images',
+    '--n_images',
     type=int,
     prompt=True,
     help='number of images to simulate'
 )
 @click.option(
-    '--image-sidelength',
+    '--image_sidelength',
     type=int,
     prompt=True,
     help='sidelength of simulated images, must be divisible by two'
 )
 @click.option(
-    '--min-defocus',
+    '--min_defocus',
     type=float,
     prompt=True,
     help='minimum defocus value in microns, positive is underfocus'
 
 )
 @click.option(
-    '--max-defocus',
+    '--max_defocus',
     type=float,
     prompt=True,
     help='maximum defocus value in microns, positive is underfocus'
 )
 @click.option(
-    '--random-seed',
+    '--random_seed',
     default=None,
     type=int,
     prompt=True,
     help='random seed for reproducing identical simulations'
 )
 @click.option(
-    '--n-gpus',
+    '--n_gpus',
     default=1,
     type=int,
     prompt=True,
@@ -149,53 +161,53 @@ def spsim_scarf(
         sleep(0.1)
     click.echo(f'done!')
 
-@click.command()
+@cli.command('spsim_local')
 @click.option(
-    '--input-directory',
+    '--input_directory',
     type=click.Path(exists=True),
     prompt=True,
     help='input directory containing structure files'
 )
 @click.option(
-    '--output-basename',
+    '--output_basename',
     type=str,
     prompt=True,
     help='basename for output files from simulation'
 )
 @click.option(
-    '--n-images',
+    '--n_images',
     type=int,
     prompt=True,
     help='number of images to simulate'
 )
 @click.option(
-    '--image-sidelength',
+    '--image_sidelength',
     type=int,
     prompt=True,
     help='sidelength of simulated images, must be divisible by two'
 )
 @click.option(
-    '--min-defocus',
+    '--min_defocus',
     type=float,
     prompt=True,
     help='minimum defocus value in microns, positive is underfocus'
 
 )
 @click.option(
-    '--max-defocus',
+    '--max_defocus',
     type=float,
     prompt=True,
     help='maximum defocus value in microns, positive is underfocus'
 )
 @click.option(
-    '--random-seed',
+    '--random_seed',
     default=None,
     type=int,
     prompt=True,
     help='random seed for reproducing identical simulations'
 )
 @click.option(
-    '--n-gpus',
+    '--n_gpus',
     default=1,
     type=int,
     prompt=True,
@@ -211,64 +223,12 @@ def spsim_local(
         random_seed,
         n_gpus,
 ):
-
-    # create simulation
-    simulation = prepare_simulation(
-        input_directory=input_directory,
-        output_basename=output_basename,
-        n_images=n_images,
-        image_sidelength=image_sidelength,
-        defocus_range=(min_defocus, max_defocus),
-        random_seed=random_seed
-    )
-
-    click.echo('\n')
-    click.echo('### SPSIM 0.0.1 ###')
-    click.echo('killing this process will terminate your simulation')
-
-    n_structure_files = len(simulation.config.structure_files)
-    click.echo(f'simulating {n_images} images from {n_structure_files} structure files')
-    click.echo(f'job walltimes are short, your jobs will not block others for long!')
-    start_time = datetime.now()
-    click.echo(f'started computations at {start_time.strftime("%m/%d/%Y, %H:%M:%S")}')
-
-    jf = f'{simulation.config.output_basename}.json'
-    with open(jf, 'w') as f:
-        f.write(simulation.json())
-    click.echo(f"simulation params stored in '{jf}'")
-
-    zf = simulation.zarr_filename
-    click.echo(f"results stored in '{zf}'\n")
-
-    click.echo(f'submitting computations to the cluster takes time')
-    click.echo(f'once all jobs are submitted, status of simulation will be printed to the console')
-    click.echo(f'\n')
-
-    simulation.create_zarr_store()
-    for i in n_images:
-        simulation.simulate_image(idx)
-
-    za = zarr.convenience.open(zf)
-
-    while za.nchunks_initialized < za.nchunks:
-        now = datetime.now()
-        particles_simulated = za.nchunks_initialized
-        particles_simulated_str = f'{particles_simulated} / {n_images} particles simulated'
-        elapsed_time = naturaldelta(now - start_time, minimum_unit='seconds')
-
-        if particles_simulated > 0:
-            time_per_particle = (now - start_time).total_seconds() / particles_simulated
-        else:
-            time_per_particle = 9999.99
-        click.echo(
-            f'{particles_simulated_str} in {elapsed_time} (avg. {time_per_particle:.2f}s per particle)        \r',
-            nl=False
-        )
-        sleep(0.1)
-    click.echo(f'done!')
+    spsim_local_call(input_directory=input_directory, output_basename=output_basename, \
+        n_images=n_images, image_sidelength=image_sidelength, min_defocus=min_defocus, \
+            max_defocus=max_defocus, random_seed=random_seed, n_gpus=n_gpus)
 
 
-@click.command()
+@cli.command('zarr2mrcs_cli')
 @click.option(
     '--input-zarr-file',
     type=click.Path(exists=True),
@@ -286,7 +246,8 @@ def zarr2mrcs_cli(input_zarr_file, mrcs_file):
     return
 
 
-@click.command()
+@cli.command('json2star_cli')
+@click.pass_context
 @click.option(
     '--input-json-file',
     type=click.Path(exists=True),
@@ -302,3 +263,10 @@ def zarr2mrcs_cli(input_zarr_file, mrcs_file):
 def json2star_cli(input_json_file, output_star_file):
     json2star(input_json_file, output_star_file)
     return
+
+
+def main():
+   cli(prog_name="cli")
+
+if __name__ == "__main__":
+    main()
--- spsim/localtesting.py
+++ spsim/localtesting.py
@@ -10,6 +10,7 @@ from humanize import naturaldelta
 from simulation_functions import prepare_simulation
 from utils import zarr2mrcs, json2star
 
+"""
 input_directory = '/home/jg/parakeet/testing/'
 output_basename = '/home/jg/parakeet/output_testing/test'
 n_images = 10
@@ -18,48 +19,52 @@ min_defocus = 1
 max_defocus = 10
 random_seed = 158
 n_gpus = 1
+"""
 
-# create simulation
-simulation = prepare_simulation(
-    input_directory=input_directory,
-    output_basename=output_basename,
-    n_images=n_images,
-    image_sidelength=image_sidelength,
-    defocus_range=(min_defocus, max_defocus),
-    random_seed=random_seed
-)
+def spsim_local_call(input_directory, output_basename, n_images,\
+    image_sidelength, min_defocus, max_defocus, random_seed, n_gpus):
 
-n_structure_files = len(simulation.config.structure_files)
-
-start_time = datetime.now()
-
-jf = f'{simulation.config.output_basename}.json'
-with open(jf, 'w') as f:
-    f.write(simulation.json())
-
-zf = simulation.zarr_filename
-
-simulation.create_zarr_store()
-for idx in range (n_images):
-    simulation.simulate_image(idx)
-
-za = zarr.convenience.open(zf)
-while za.nchunks_initialized < za.nchunks:
-    now = datetime.now()
-    particles_simulated = za.nchunks_initialized
-    particles_simulated_str = f'{particles_simulated} / {n_images} particles simulated'
-    elapsed_time = naturaldelta(now - start_time, minimum_unit='seconds')
-
-    if particles_simulated > 0:
-        time_per_particle = (now - start_time).total_seconds() / particles_simulated
-    else:
-        time_per_particle = 9999.99
-    print(
-        f'{particles_simulated_str} in {elapsed_time} (avg. {time_per_particle:.2f}s per particle)        \r',
-        nl=False
+    # create simulation
+    simulation = prepare_simulation(
+        input_directory=input_directory,
+        output_basename=output_basename,
+        n_images=n_images,
+        image_sidelength=image_sidelength,
+        defocus_range=(min_defocus, max_defocus),
+        random_seed=random_seed
     )
-    sleep(0.1)
 
-zarr2mrcs(output_basename+'.zarr', output_basename+'.mrc')
-json2star(output_basename+'.json', output_basename+'.star')
-print(f'done!')
+    n_structure_files = len(simulation.config.structure_files)
+
+    start_time = datetime.now()
+
+    jf = f'{simulation.config.output_basename}.json'
+    with open(jf, 'w') as f:
+        f.write(simulation.json())
+
+    zf = simulation.zarr_filename
+
+    simulation.create_zarr_store()
+    for idx in range (n_images):
+        simulation.simulate_image(idx)
+
+    za = zarr.convenience.open(zf)
+    while za.nchunks_initialized < za.nchunks:
+        now = datetime.now()
+        particles_simulated = za.nchunks_initialized
+        particles_simulated_str = f'{particles_simulated} / {n_images} particles simulated'
+        elapsed_time = naturaldelta(now - start_time, minimum_unit='seconds')
+
+        if particles_simulated > 0:
+            time_per_particle = (now - start_time).total_seconds() / particles_simulated
+        else:
+            time_per_particle = 9999.99
+        print(
+            f'{particles_simulated_str} in {elapsed_time} (avg. {time_per_particle:.2f}s per particle)        \r',
+            nl=False
+        )
+        sleep(0.1)
+
+    zarr2mrcs(output_basename+'.zarr', output_basename+'.mrc')
+    json2star(output_basename+'.json', output_basename+'.star')
+    print(f'done!')
